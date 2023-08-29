4bd78dbbfa39e09b35d3cc8cd3ecbc001a96a64a
Antonin <antoninploquin@hotmail.fr>
2023-08-29 19:20:25 +0200

    thrid commit

---
+++ constants/data.js
@@ -0,0 +1,106 @@
+const userData = {
+  githubUsername: "Antoninplo",
+  name: "Antonin Ploquin",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "antoninploquin@gmail.com",
+  phone: "+33 6 37 57 73",
+  address: "Paris, France.",
+  projects: [
+    {
+      title: "Mymoviz",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/Mymoviz.png",
+    },
+    {
+      title: "Morning News",
+      link: "https://placeholdertech.in",
+      imgUrl: "/Morning News.png",
+    },
+    {
+      title: "Locapic",
+      link: "https://manuarora.in",
+      imgUrl: "/Locapic.gif",
+    },
+    {
+      title: "FaceUp",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/FaceUp.gif",
+    },
+    {
+      title: "Déj'Friend",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/Accueil _ Restos.png",
+    },
+  ],
+  about: {
+    title:
+      "Développeur Fullstack web et mobile sur Javascript et React",
+    description: [
+      `🇫🇷 Passionné par la tech mais aussi par la création en générale, j'ai effectué ma reconversion professionnelle en commençant par un bootcamp de code à La Capsule où j'ai pu apprendre les bases de la programmation sur Javascript.`,
+      `Grâce à ce cursus, j'ai eu l'occasion de mettre en pratique mes connaissances fraichement acquises lors de la création d'un MVP. Nom de code Déj'Friend. Il s'agit d'un application mobile type réseaux sociaux développée sur React Native.`,
+      `À l'heure actuelle, Déj'Friend est encore en cours de développement mais nous comptons bien finir par la mettre en ligne. Pour l'heure, il me faut trouver un stage, un CDI ou même des missions en freelance.`,
+    ],
+    currentProject: "Déj'Friend",
+    currentProjectUrl: " ",
+  },
+  experience: [
+    {
+      title: "Frontend developper",
+      company: "Déj'Friend",
+      year: "2023",
+      companyLink: "",
+      desc: "Intégration de la maquette - Création d'écrans à choix multiples - Branchement avec le backend - Optimisation de l'UX",
+    },
+    {
+      title: "Apprenti à La Capsule",
+      company: "La Capsule",
+      year: "2023",
+      companyLink: "https://www.lacapsule.academy/",
+      desc: "Apprentissage intensif des rudiments de la programmation web et mobile",
+    },
+    {
+      title: "Journaliste",
+      company: "Éditions Lariviere",
+      year: "2021-2023",
+      companyLink: "https://www.editions-lariviere.fr/",
+      desc: "Rédaction d'articles, Essai produits, Reportage vidéo",
+    },
+    {
+      title: "Chef de projet",
+      company: "Focus Home Entertainement",
+      year: "2021",
+      companyLink: "https://www.focus-entmt.com/fr",
+      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
+    },
+    {
+      title: "Diplome Chef de projet",
+      company: "ISART Digital.",
+      year: "2020",
+      companyLink: "https://www.isart.fr/",
+      desc: "Obtention du titre RNCP niveau 7 (équivalent bac +5) dans la gestion de projet vidéo ludique.",
+    },
+    {
+      title: "Assistant Marketing & Communication",
+      company: "Groupe Chapat.",
+      year: "2017-2019",
+      companyLink: "https://www.troopy.com/2023/01/17/lhistoire-du-groupe-chapat/",
+      desc: "En charge du CRM pour toutes les concessions motos du groupe (BMW, Triumph, Yamaha, Ducati).",
+    },
+    {
+      title: "Assistant Compétition",
+      company: "Yamaha Motor",
+      year: "2019",
+      companyLink: "https://www.yamaha-motor.eu/",
+      desc: "J'ai aidé le team usine à toute la préparation du rallye Dakar.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1ee80Xf50GQhVl-Y0KG6E0_QcmhzMqmhQ/view?usp=drive_link",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/antonin-ploquin/",
+    github: "https://github.com/Antoninplo",
+  },
+};
+
+export default userData;
